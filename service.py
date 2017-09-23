60c2522c2fdd87109fcbe7b0fcac4e9e7bd95b57
firsttris <tristanteufel@googlemail.com>
2017-09-23 11:32:18 +0200

    fix encoding issue

--- service.py
+++ service.py
@@ -7,14 +7,15 @@ import xbmcgui
 import xbmcplugin
 
 my_addon = xbmcaddon.Addon('plugin.video.sendtokodi')
-addon_dir = xbmc.translatePath( my_addon.getAddonInfo('path') )
-
-sys.path.append(os.path.join( addon_dir, 'youtube-dl' ) )
+addon_dir = xbmc.translatePath(my_addon.getAddonInfo('path'))
+sys.path.append(os.path.join(addon_dir, 'youtube-dl'))
 
 from youtube_dl import YoutubeDL
 
+
 class replacement_stderr(sys.stderr.__class__):
     def isatty(self): return False
+
 
 sys.stderr.__class__ = replacement_stderr
 
@@ -23,27 +24,35 @@ __url__ = sys.argv[0]
 # Get the plugin handle as an integer number.
 __handle__ = int(sys.argv[1])
 
+
 def debug(content):
     log(content, xbmc.LOGDEBUG)
 
+
 def notice(content):
     log(content, xbmc.LOGNOTICE)
+
 
 def log(msg, level=xbmc.LOGNOTICE):
     addon = xbmcaddon.Addon()
     addonID = addon.getAddonInfo('id')
     xbmc.log('%s: %s' % (addonID, msg), level)
 
+
 def showInfoNotification(message):
     xbmcgui.Dialog().notification("SendToKodi", message, xbmcgui.NOTIFICATION_INFO, 5000)
 
+
 def showErrorNotification(message):
-    xbmcgui.Dialog().notification("SendToKodi", message, xbmcgui.NOTIFICATION_ERROR, 5000)
+    xbmcgui.Dialog().notification("SendToKodi", message,
+                                  xbmcgui.NOTIFICATION_ERROR, 5000)
+
 
 def getParams():
     paramstring = sys.argv[2]
     cleanedparams = paramstring[1:]
     return cleanedparams
+
 
 ydl_opts = {
     'format': 'best'
@@ -56,13 +65,14 @@ ydl.add_default_info_extractors()
 
 
 with ydl:
+    showInfoNotification("resolving stream(s)")
     result = ydl.extract_info(url, download=False)
 if 'entries' in result:
-    # Can be a playlist or a list of videos
+    # Playlist
     pl = xbmc.PlayList(1)
     pl.clear()
     for video in result['entries']:
-        debug('Video #%d: %s' % (video['playlist_index'], video['title']))
+        debug(video)
         url = video['url']
         thumbnail = video['thumbnail']
         title = video['title']
@@ -71,18 +81,16 @@ if 'entries' in result:
         play_item.setArt({'thumb': thumbnail})
         xbmc.PlayList(1).add(url, play_item)
     xbmc.Player().play(pl)
+    showInfoNotification("playing playlist " + result['title'])
 else:
     # Just a video
     debug(result)
     title = result['title']
-    debug("Title: " + title)
     thumbnail = result['thumbnail']
-    debug("Thumbnail: " + thumbnail)
     url = result['url']
-    debug("Url: " + url)
     play_item = xbmcgui.ListItem(title, path=url)
     play_item.setInfo(type='Video', infoLabels={'Title': title})
     play_item.setArt({'thumb': thumbnail})
     # Pass the item to the Kodi player.
-    showInfoNotification("Playing... " + title)
+    showInfoNotification("playing title " + title)
     xbmcplugin.setResolvedUrl(__handle__, True, listitem=play_item)
