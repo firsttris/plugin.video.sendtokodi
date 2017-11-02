34edfee8d1e87fc1bafb5a850c10634d5fe5f4f8
Dan LaManna <me@danlamanna.com>
2017-11-02 10:40:31 -0400

    Refactor common logic into getPlayItemFromVideo

--- service.py
+++ service.py
@@ -49,6 +49,17 @@ def getParams():
     return cleanedparams
 
 
+def getPlayItemFromVideo(video):
+    debug(video)
+    url = video['url']
+    thumbnail = video['thumbnail']
+    title = video['title']
+    play_item = xbmcgui.ListItem(title, path=url)
+    play_item.setInfo(type='Video', infoLabels={'Title': title})
+    play_item.setArt({'thumb': thumbnail})
+    return play_item
+
+
 ydl_opts = {
     'format': 'best'
 }
@@ -62,30 +73,16 @@ ydl.add_default_info_extractors()
 with ydl:
     showInfoNotification("resolving stream(s)")
     result = ydl.extract_info(url, download=False)
+
 if 'entries' in result:
     # Playlist
     pl = xbmc.PlayList(1)
     pl.clear()
     for video in result['entries']:
-        debug(video)
-        url = video['url']
-        thumbnail = video['thumbnail']
-        title = video['title']
-        play_item = xbmcgui.ListItem(title, path=url)
-        play_item.setInfo(type='Video', infoLabels={'Title': title})
-        play_item.setArt({'thumb': thumbnail})
-        xbmc.PlayList(1).add(url, play_item)
+        xbmc.PlayList(1).add(url, getPlayItemFromVideo(video))
     xbmc.Player().play(pl)
     showInfoNotification("playing playlist " + result['title'])
 else:
-    # Just a video
-    debug(result)
-    title = result['title']
-    thumbnail = result['thumbnail']
-    url = result['url']
-    play_item = xbmcgui.ListItem(title, path=url)
-    play_item.setInfo(type='Video', infoLabels={'Title': title})
-    play_item.setArt({'thumb': thumbnail})
-    # Pass the item to the Kodi player.
-    showInfoNotification("playing title " + title)
-    xbmcplugin.setResolvedUrl(__handle__, True, listitem=play_item)
+    # Just a video, pass the item to the Kodi player.
+    showInfoNotification("playing title " + result['title'])
+    xbmcplugin.setResolvedUrl(__handle__, True, listitem=getPlayItemFromVideo(result))
