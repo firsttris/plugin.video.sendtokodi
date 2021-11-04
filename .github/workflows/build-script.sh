648c6528a623f50e799df2009cec9a6c6e0cd026
firsttris <tristanteufel@googlemail.com>
2021-11-04 22:21:59 +0100

    migrate travis to github actions

---
+++ .github/workflows/build-script.sh
@@ -0,0 +1,37 @@
+rm -rf .git/
+# LATER envsubst < "addon.template.xml" > "addon.xml"
+cd ..
+# youtube_dl
+wget ${YOUTUBE_DL}
+unzip master.zip
+cp -R youtube-dl-master/youtube_dl ${ADDON_NAME}
+# clone repo
+git clone https://${GH_REPO}
+git clone https://${GH_REPO_PYTHON3}
+# package python 2 addon
+envsubst < "${REPO_NAME}/${ADDON_NAME}/addon.template.xml" > "${REPO_NAME}/${ADDON_NAME}/addon.xml" # python 2 addon.xml
+cp ${REPO_NAME}/${ADDON_NAME}/addon.xml ${ADDON_NAME} # copy python 2 addon.xml to addon folder
+zip -r ${ADDON_NAME}-${VERSION}.zip ${ADDON_NAME} # create zip for python 2
+cp ${ADDON_NAME}-${VERSION}.zip ${REPO_NAME}/${ADDON_NAME}/ # copy source code to python 2 repo
+# package python 3 addon
+envsubst < "${REPO_NAME_PYTHON3}/${ADDON_NAME}/addon.template.xml" > "${REPO_NAME_PYTHON3}/${ADDON_NAME}/addon.xml" #python 3 addon.xml
+rm ${ADDON_NAME}/addon.xml # remove previous python 2 addon.xml
+cp ${REPO_NAME_PYTHON3}/${ADDON_NAME}/addon.xml ${ADDON_NAME} # copy python 3 addon.xml to addon folder
+zip -r ${ADDON_NAME}-${VERSION}.zip ${ADDON_NAME} # create zip for python 3
+cp ${ADDON_NAME}-${VERSION}.zip ${REPO_NAME_PYTHON3}/${ADDON_NAME}/ # copy source code to python 3 repo
+# commit & push to python 2 repo
+cd ${REPO_NAME}/
+envsubst < "addon.template.xml" > "addon.xml"
+md5sum addon.xml > addon.xml.md5
+git add .
+git commit -m "Travis-CI Update"
+git push --force --quiet "https://${GH_REPO}" master > /dev/null 2>&1
+# go back one folder
+cd ..
+# commit & push to python 3 repo
+cd ${REPO_NAME_PYTHON3}/
+envsubst < "addon.template.xml" > "addon.xml"
+md5sum addon.xml > addon.xml.md5
+git add .
+git commit -m "Travis-CI Update"
+git push --force --quiet "https://${GH_REPO_PYTHON3}" master > /dev/null 2>&1
