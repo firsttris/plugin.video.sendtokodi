ef7b45b6d6a2a6bb94997278c1c677079b8b2572
47Ohms <203828787+47Ohms@users.noreply.github.com>
2025-04-03 09:00:44 +0100

    Open settings when no parameters are passed

--- service.py
+++ service.py
@@ -320,7 +320,12 @@ def playlistIndex(url, playlist):
     
     return None
 
-
+# Open the settings if no parameters have been passed. Prevents crash.
+# This happens when the addon is launched from within the Kodi OSD.
+if not sys.argv[2]:
+    xbmcaddon.Addon().openSettings()
+    exit()
+    
 # Use the chosen resolver while forcing to use youtube_dl on legacy python 2 systems (dlp is python 3.6+)
 if xbmcplugin.getSetting(int(sys.argv[1]),"resolver") == "0" or sys.version_info[0] == 2:
     from youtube_dl import YoutubeDL
