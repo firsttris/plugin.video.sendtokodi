637ab2d9fd9b73b4b21ea2b0b4e6a12aae457751
brevilo <brevilo@users.noreply.github.com>
2025-04-07 21:24:51 +0200

    Adjust CI for Leia

--- .github/scripts/build-addon.sh
+++ .github/scripts/build-addon.sh
@@ -2,7 +2,6 @@
 set -e
 
 VERSION=$1
-KODI_TARGET=$2
 
 # check if version bump failed (will be "undefined" according to mathieudutour/github-tag-action@v5.6)
 if [[ "$VERSION" == "undefined" || -z "$VERSION" ]]; then
@@ -10,26 +9,9 @@ if [[ "$VERSION" == "undefined" || -z "$VERSION" ]]; then
     exit 190
 fi
 
-# based on the targeting kodi version different adjustments must be made. Further information here: https://kodi.wiki/view/Addon.xml
-# xmlstarlet or a python xml wrapper would be more robust than sed
-case $KODI_TARGET in
-  "Leia")
-    echo -n "TODO"
-    $GITHUB_WORKSPACE/.github/scripts/addon_xml_adjuster.py --plugin-version $VERSION --xbmc-python "2.25.0"
-    build_folder=Leia
-    ;;
+$GITHUB_WORKSPACE/.github/scripts/addon_xml_adjuster.py --plugin-version $VERSION
+mkdir $RUNNER_TEMP
 
-  "Matrix" | "Nexus")
-    $GITHUB_WORKSPACE/.github/scripts/addon_xml_adjuster.py --plugin-version $VERSION --xbmc-python "3.0.0"
-    build_folder=Matrix
-    ;;
-  *)
-    echo -n "Unknown kodi version as target. Aborting build."
-    exit 191
-    ;;
-esac
-
-mkdir $RUNNER_TEMP/${build_folder}
 # create zip file. The file needs to include the plugin folder itself  
 cd ..
-zip -r $RUNNER_TEMP/${build_folder}/plugin.video.sendtokodi-$VERSION.zip plugin.video.sendtokodi -x "*.git*" # create zip
+zip -r $RUNNER_TEMP/plugin.video.sendtokodi-$VERSION.zip plugin.video.sendtokodi -x "*.git*" # create zip
