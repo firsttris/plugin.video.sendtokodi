aa93a6be0f2b066f41cd6361a423e9a45de1e1d1
brevilo <brevilo@users.noreply.github.com>
2025-04-23 11:08:07 +0200

    Fix repository's addon.xml adjustment workflow (#147)
    * Fix repository's addon.xml adjustment workflow
    
    - Don't just update the version, update the entire addon element
    - Avoids update anomalies because the template can't get outdated
    - Depends on the template not containing a redundant addon element
    
    * Retain VERSION (for commit message)
    
    Also fixed some trailing whitespaces
    
    ---------
    
    Co-authored-by: = <=>

--- .github/scripts/publish-addon.sh
+++ .github/scripts/publish-addon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-# this script publishes the addon zip file to the unofficial python2/3 kodi repo 
+# this script publishes the addon zip file to the unofficial python2/3 kodi repo
 
 VERSION=$1
 REPO_URL=$2
@@ -14,16 +14,17 @@ if [[ $GITHUB_REPOSITORY == "firsttris/plugin.video.sendtokodi" ]]; then
     # add the plugin zip file
     mv plugin.video.sendtokodi-$VERSION.zip $REPO_FOLDER/plugin.video.sendtokodi/
 
-    # Update repository addon xml (not the plugin addon.xml) to include the latest version of sendtokodi
+    # Update repository's addon.xml with the addon's addon.xml
+    $GITHUB_WORKSPACE/.github/scripts/update-repo-xml.py --repo-root $REPO_FOLDER
     cd $REPO_FOLDER
-    envsubst < "addon.template.xml" > "addon.xml"  
     md5sum addon.xml > addon.xml.md5
+
     # Add new addon zip file, the repo addon.xml, its md5 hash file and then commit and push
     git config --global user.name "github-actions[bot]"
     git config --global user.email "github-actions[bot]@users.noreply.github.com"
     git add .
     git commit -m "CI Update for $VERSION"
     git push --force --quiet "https://firsttris:$TOKEN@$REPO_URL" master
-else 
+else
     echo "Not in the main repo, build will not be published."
 fi
